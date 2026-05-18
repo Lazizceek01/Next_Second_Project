cb9025e24f5c161e50132f3f85b25572ff28c163
Laziz Azizov <azizovlaziz33@gmail.com>
2026-05-18 11:17:48 +0500

    Any commit

--- pratice/app/api/products/[id]/route.ts
+++ pratice/app/api/products/[id]/route.ts
@@ -5,6 +5,7 @@ type Params = {
 	params: Promise<{id: string}>;
 };
 
+
 export async function GET(req: Request, {params}: Params){
 	const {id} = await params;
 	const product = products.find((p) => p.id == +id);
@@ -12,3 +13,30 @@ export async function GET(req: Request, {params}: Params){
 	return NextResponse.json(product);
 }
 
+
+// Short form
+export async function DELETE(req: Request, {params}: Params){
+	const {id} = await params;
+	console.log(id);
+	const index = products.findIndex((p) => p.id == +id);
+	console.log(index);
+	products.splice(index, 1)
+	console.log(products);
+	return NextResponse.json({message: "Product muvaffaqiyatli ochirildi"});
+}
+
+
+// Create PUT typical and short form
+export async function PUT(req: Request, { params }: Params) {
+  const { id } = await params;
+  const { title, price } = await req.json();
+
+	const product = products.find((p) => p.id == +id);
+
+  if (product) {
+    product.title = title;
+    product.price = price;
+  }
+
+  return NextResponse.json({ message: "Product muvaffaqiyatli yangilandi", product });
+}
